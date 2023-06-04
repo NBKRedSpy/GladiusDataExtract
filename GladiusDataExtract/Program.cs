51068f9a6275826f8f565ee40f15472861178f1e
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-06-04 09:53:50 -0400

    Add project files.

---
+++ GladiusDataExtract/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GladiusDataExtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        public static void ExtractWeapons(string folderName, string outputFile)
+        {
+
+        }
+    }
+}
