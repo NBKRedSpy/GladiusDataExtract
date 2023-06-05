9edf3efda0e1c310814c2191b056497c30c6c31f
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-06-05 17:51:18 -0400

    wip

---
+++ GladiusDataExtract/Weapons/Effect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladiusDataExtract.Weapons
+{
+	/// <summary>
+	/// The effect of a weapon.  Ex:  attacks
+	/// </summary>
+	/// <param name="Name"></param>
+	/// <param name="Modifiers"></param>
+	public record Effect(string Name, List<ModifierType> Modifiers)
+	{
+		/// <summary>
+		/// Runs all the modifiers for this effect.
+		/// </summary>
+		/// <param name="unitAttributeValue"></param>
+		/// <returns></returns>
+		public decimal ApplyModifiers(decimal unitAttributeValue)
+		{
+			decimal result = unitAttributeValue;
+
+			//Note - Almost all the modifiers have a single operation.  
+			//  However, ClusterMines.xml has a min and a max on the same effect.
+			foreach (ModifierType modifier in Modifiers)
+			{
+				result = modifier.ApplyModifier(result);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ GladiusDataExtract/Weapons/ModifierType.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladiusDataExtract.Weapons
+{
+	/// <summary>
+	/// A modifier for an effect.  For example, add 2 more attacks or add 12 range damage.
+	/// </summary>
+	/// <param name="Type"></param>
+	/// <param name="Value"></param>
+	public record ModifierType(string Type, decimal Value)
+	{
+
+		public decimal ApplyModifier(decimal? attributeValue)
+		{
+			return ApplyModifier(Type, Value, attributeValue);
+		}
+
+		/// <summary>
+		/// Applies a modfier command to a value.  For example:  "Add 2" to the value.
+		/// Ex: add 2 to value.
+		/// </summary>
+		/// 
+		/// <param name="attributeValue"></param>
+		/// <returns></returns>
+		public decimal ApplyModifier(string type, decimal sourceValue, decimal? attributeValue)
+		{
+			decimal attValue = attributeValue ?? 0;
+
+			switch (type)
+			{
+				case "add":
+					return attValue += sourceValue;
+				case "base":
+					//Don't know what this is.  It seems to always be zero.
+					//For now, returning the unit's value.
+					return attValue;
+				case "mul":
+					return (sourceValue + 1) * attValue;
+				case "min":
+					//Not sure.  Is it the min between the two?
+					return Math.Min(attValue, sourceValue);
+				case "max":
+					//Not sure.  Is it the max between the two?
+					return Math.Max(attValue, sourceValue);
+				default:
+					throw new ArgumentException($"Unexpected value for type: '{type}'", "type");
+			}
+		}
+	}
+}
