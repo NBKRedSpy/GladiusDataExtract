7a7fd981bf84c800266f6ed2a572ef6c33666809
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-06-08 17:32:19 -0400

    WIP

---
+++ GladiusDataExtract/Extract/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GladiusDataExtract.Entities;
+using du = GladiusDataExtract.Extract.Units;
+using dw = GladiusDataExtract.Extract.Weapons;
+
+namespace GladiusDataExtract.Extract
+{
+	/// <summary>
+	/// A class to convert DTO's to project entities.
+	/// </summary>
+	public class Converter
+	{
+
+		
+		public List<Unit> ConvertUnits(List<du.Unit> dtoUnits)
+		{
+
+
+
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ GladiusDataExtract/Extract/LanguageExtract.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GladiusDataExtract.Extract
+{
+    public class LanguageExtract
+    {
+
+        /// <summary>
+        /// Parses a localization text file in the "&ltentry name= value=" format
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> GetTextStrings(string path)
+        {
+
+            string xml = File.ReadAllText(path);
+
+            //Some lines are not formatted correctly.  Sometimes in flavor text or not, but it seems to always follow
+            //this format:
+            //<entry name="AstraMilitarum/Headquarters" value="<string name='Buildings/AstraMilitarum/Headquarters'/>"/>
+            //A hack is to replace the start and end of the bad text.
+
+            xml = xml
+                .Replace("value=\"<string name='", "value=\"") //Start
+                .Replace("'/>\"/>", "\"/>"); //End
+
+
+            XmlDocument xmlDocument = new XmlDocument();
+
+            xmlDocument.LoadXml(xml);
+
+            Dictionary<string, string> textLookup = new();
+
+            foreach (XmlNode xmlNode in xmlDocument.SelectNodes("language/entry")!)
+            {
+                //For some reason, there are duplicates. 
+                //The earlier items seem to be text, while the latter are keys.
+                //Examples:
+                //	<entry name="FlamerFlavor" value="Flamers are short-ranged weapons that spew out highly volatile clouds of liquid chemicals that ignite on contact with air. They are primarily used to scour the enemy from defended positions, as walls are of no defence against blasts of superheated vapour."/>
+                //
+                //<entry name="BaleflamerFlavor" value="<string name='Weapons/FlamerFlavor'/>"/>
+                textLookup.TryAdd(xmlNode.Attributes!["name"]!.Value, xmlNode.Attributes!["value"]!.Value);
+            }
+
+            return textLookup;
+        }
+    }
+}
