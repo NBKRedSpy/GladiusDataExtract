d3c2a1a684727eb2b581272a67865b52c0ecb30d
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-06-08 17:15:36 -0400

    WIP

--- GladiusDataExtract/Entities/Unit.cs
+++ GladiusDataExtract/Entities/Unit.cs
@@ -11,15 +11,34 @@ namespace GladiusDataExtract.Entities
 	/// </summary>
 	public class Unit
 	{
-		string Name { get; set; } = "";
-		string Key { get; set; } = "";
+		/// <summary>
+		/// Friendly name of the unit
+		/// </summary>
+		public string Name { get; set; } = "";
 
-		int Armor { get; set; } = 0;
-		int Hitpoints { get; set; } = 0;
-		int Movement { get; set; } = 0;
-		int ProductionCost { get; set; } = 0;
-		int ModelCount { get; set; } = 0;
-	}
+		/// <summary>
+		/// The internal key of the unit.  Ex:  Tyranids/Hormagaunt
+		/// </summary>
+		public string Key { get; set; } = "";
+
+		public int Armor { get; set; } = 0;
+
+		/// <summary>
+		/// The hit points per model.  Ex: a 8 model unit at 2 hp.
+		/// </summary>
+		public int Hitpoints { get; set; } = 0;
+		public int Movement { get; set; } = 0;
+
+		/// <summary>
+		/// The number of models in a unit.  Ex:  A unit may have 8 soldiers.
+		/// </summary>
+		public int ModelCount { get; set; } = 0;
+
+		public int ProductionCost { get; set; } = 0;
+
+		public Resources ProductionResources { get; set; } = new Resources();
+
+    }
 
 
 
--- GladiusStatWeb/Services/GladiusDataService.cs
+++ GladiusStatWeb/Services/GladiusDataService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using GladiusDataExtract;
+using GladiusDataExtract.Extract;
 using GladiusDataExtract.Extract.Units;
 using GladiusDataExtract.Extract.Weapons;
 using gde = GladiusDataExtract;
@@ -18,20 +20,23 @@ namespace GladiusStatWeb.Services
             //todo:  change to configure in settings or env.
 
 
+            var extractor = new Extractor();
+
+
             LanguageExtract languageExtract = new LanguageExtract();
 
             Dictionary<string,string> weaponLocalizationText = languageExtract.GetTextStrings("D:\\Games\\Steam\\steamapps\\common\\Warhammer 40000 Gladius - Relics of War\\Data\\Core\\Languages\\English\\Weapons.xml");
 
             List<Weapon> weapons = new();
             
-            weapons = gde.Program.ExtractWeaponInfo(@"D:\Games\Steam\steamapps\common\Warhammer 40000 Gladius - Relics of War\Data\World\Weapons", weaponLocalizationText);
+            weapons = extractor.ExtractWeaponInfo(@"D:\Games\Steam\steamapps\common\Warhammer 40000 Gladius - Relics of War\Data\World\Weapons", weaponLocalizationText);
 
 
 			Dictionary<string, string> unitLocalizationText = languageExtract.GetTextStrings("D:\\Games\\Steam\\steamapps\\common\\Warhammer 40000 Gladius - Relics of War\\Data\\Core\\Languages\\English\\Units.xml");
 
 			Dictionary<string, Weapon> weaponLookup = weapons.ToDictionary(x => x.Key );
 
-            GladiusUnits = gde.Program.ExtractUnitInfo(
+            GladiusUnits = extractor.ExtractUnitInfo(
                 @"D:\Games\Steam\steamapps\common\Warhammer 40000 Gladius - Relics of War\Data\World\Units",
                 weaponLookup, unitLocalizationText);
         }
